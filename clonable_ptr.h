df87901d41c0ac9a2cef4476e7fbe59ad6146f9f
Lulu Han <33955564+hanlulugeren@users.noreply.github.com>
2018-05-18 19:23:39 +0800

    Add a smart pointer--clonable_ptr
    This smart pointer allows copy constructor and assignment constructor.

---
+++ clonable_ptr.h
@@ -0,0 +1,39 @@
+#ifndef SMARTPTR_MEMBER_H
+#define SMARTPTR_MEMBER_H
+
+
+/// \brief A pointer which can be copied and cloned
+/// \tparam T class or type
+/// \details \p T should adhere to the \p Clonable interface
+template<class T> 
+class clonable_ptr : public member_ptr<T>
+{
+public:
+	clonable_ptr(const T &obj) 
+		: member_ptr<T>(obj.Clone()) 
+	{
+	}
+
+	clonable_ptr(T *p = NULLPTR) 
+		: member_ptr<T>(p) 
+	{
+	}
+
+	clonable_ptr(const clonable_ptr<T>& rhs)
+		: member_ptr<T>(rhs.m_p ? rhs.m_p->Clone() : nullptr) 
+	{
+	}
+
+	clonable_ptr<T>& operator=(const clonable_ptr<T>& rhs);
+};
+
+template <class T> 
+clonable_ptr<T>& clonable_ptr<T>::operator=(const clonable_ptr<T>& rhs)
+{
+	T *old_p = this->m_p;
+	this->m_p = rhs.m_p ? rhs.m_p->Clone() : nullptr;
+	delete old_p;
+	return *this;
+}
+
+#endif//SMARTPTR_MEMBER_H
